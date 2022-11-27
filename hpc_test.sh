197abec63df10ccc01268fabac765a278ebae92b
s183898 <59825652+s183898@users.noreply.github.com>
2022-11-27 13:26:49 +0100

    for hpc test

---
+++ hpc_test.sh
@@ -0,0 +1,45 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q hpc
+### -- set the job Name -- 
+#BSUB -J My_Application
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
+#BSUB -M 3GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u your_email_address
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
+
+# here follow the commands you want to execute with input.in as the input file
+### load virtual environment 
+
+source venv_env/bin/activate
+
+### load modules 
+module load python3/3.7.11
+module load cuda/1.11
+module load numpy 
+
+### if python packages not avail (module avail package)
+### python3 -m pip install package_name
+
+### run python files 
+
+mypythonfile.py
